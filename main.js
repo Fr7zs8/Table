f52e2a6d54c90672bfb1f04890f964a2f5e696be
sipos.fruzsina <siposfruzsina07@gmail.com>
2024-10-11 08:43:58 +0200

    Dinamikusan bovul a tablazat

--- main.js
+++ main.js
@@ -58,61 +58,59 @@ tableheaderrow.appendChild(tableRowPet);
 const tablebody = document.createElement('tbody');
 table.appendChild(tablebody);
 
-
-for(const person of array){
-    const tr = document.createElement('tr')
-    tr.addEventListener('click', function(e){
-        const SelectedRow = tablebody.querySelector('.selected');
-        e.currentTarget.classList.add('selected');
-        if(SelectedRow  != undefined){
-            SelectedRow.classList.remove('selected');
+RenderTable();
+function RenderTable(){
+    for(const person of array){
+        const tr = document.createElement('tr')
+        tr.addEventListener('click', function(e){
+            const SelectedRow = tablebody.querySelector('.selected');
+            e.currentTarget.classList.add('selected');
+            if(SelectedRow  != undefined){
+                SelectedRow.classList.remove('selected');
+            }
+    
+            console.log('click');
+        })
+    
+        const td = document.createElement('td');
+        td.innerHTML = person.lastname;
+        tr.appendChild(td);
+    
+        const firstname1td = document.createElement('td');
+        firstname1td.innerHTML = person.firstname1;
+        tr.appendChild(firstname1td);
+    
+        if(person.firstname2 === undefined){
+            firstname1td.colSpan = 2;
+    
         }
-
-        console.log('click');
-    })
-
-    const td = document.createElement('td');
-    td.innerHTML = person.lastname;
-    tr.appendChild(td);
-
-    const firstname1td = document.createElement('td');
-    firstname1td.innerHTML = person.firstname1;
-    tr.appendChild(firstname1td);
-
-    if(person.firstname2 === undefined){
-        firstname1td.colSpan = 2;
-
-    }
-    else{
-        const firstname2td = document.createElement('td');
-        firstname2td.innerHTML = person.firstname2;
-        tr.appendChild(firstname2td);
-    }
-
-    const married = document.createElement('td');
-    //married.innerHtml?"igen":"nem"
-    if(person.married === true){
-        married.innerHTML = "igen";
-    }
-    else{
-        married.innerHTML = "nem";
-    }
-    tr.appendChild(married)
+        else{
+            const firstname2td = document.createElement('td');
+            firstname2td.innerHTML = person.firstname2;
+            tr.appendChild(firstname2td);
+        }
     
-    const allat = document.createElement('td');
-    allat.innerHTML = person.pet;
-    tr.appendChild(allat);
-
-    tablebody.appendChild(tr);
-
-
+        const married = document.createElement('td');
+        //married.innerHtml?"igen":"nem"
+        if(person.married === true){
+            married.innerHTML = "igen";
+        }
+        else{
+            married.innerHTML = "nem";
+        }
+        tr.appendChild(married)
+        
+        const allat = document.createElement('td');
+        allat.innerHTML = person.pet;
+        tr.appendChild(allat);
     
-
+        tablebody.appendChild(tr);
+    }
 }
-
 
 const form = document.getElementById("form");
 form.addEventListener('submit', function(e){
+    tablebody.innerHTML= '';
     e.preventDefault();
     const lastname = document.getElementById("lastname");
     const firstname1 = document.getElementById("firstname1");
@@ -122,9 +120,13 @@ form.addEventListener('submit', function(e){
 
     const lastnameValue = lastname.value;
     const firstname1Value= firstname1.value;
-    const firstname2Value = firstname2.value;
+    let firstname2Value = firstname2.value;
     const marriedValue = married.checked;
     const petValue = pet.value;
+
+    if(firstname2Value === ''){
+        firstname2Value = undefined; 
+    }
 
     array.push({
         lastname: lastnameValue,
@@ -135,6 +137,7 @@ form.addEventListener('submit', function(e){
     })
 
     console.log(array);
+    RenderTable();
 
 })
 
